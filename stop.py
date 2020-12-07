a99be7b1585e400bae9ab9bcdfbd32e6a3021c64
Adrian <bakaev.98@bk.ru>
2020-12-07 04:23:14 -0800

    d

--- stop.py
+++ stop.py
@@ -4,6 +4,6 @@ from std_msgs.msg import Float64, Bool
 from geometry_msgs.msg import Twist
 pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 velocity = Twist()
-velocity.angular.z = up
-velocity.linear.x = 0.15 - 0.1*abs(up)
+velocity.angular.z = 0
+velocity.linear.x = 0
 pub_vel.publish(velocity)
