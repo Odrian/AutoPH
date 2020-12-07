385abd1cd76800f61ad7b3a9eff809e934e7c36a
Adrian <adrian46tver@bk.ru>
2020-12-07 16:21:52 +0300

    d

--- light.py
+++ light.py
@@ -4,20 +4,16 @@ from time import sleep
 from std_msgs.msg import Float64, Bool
 from blobs_msgs.msg import BlobArray
 from geometry_msgs.msg import Twist
-pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+pub_vel = rospy.Publisher('vel', Twist, queue_size=1)
 
-move_flag = True
-integral = 0
-def cbError(error):
-	rospy.loginfo(error)
+def Error(msg):
+	
 
-def visError(msg):
-	rospy.loginfo(msg.blobs)
 
 if __name__ == '__main__':
+	rospy.loginfo('Test')
 	rospy.init_node('line_control')
-#	rospy.Subscriber('/error_lane', Float64, cbError, queue_size=1)
-	rospy.Subscriber('/new_blobs', BlobArray, visError, queue_size=1)
+	rospy.Subscriber('/new_blobs', BlobArray, Error, queue_size=1)
 	while not rospy.is_shutdown():
 		try:
 			rospy.sleep(0.1)
